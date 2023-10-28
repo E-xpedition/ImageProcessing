7e30018f2aab33ffd2e39e277ba313c0dd65b54b
E-xpedition <149251324+E-xpedition@users.noreply.github.com>
2023-10-28 22:08:22 +0200

    Update IMGcrop.py

--- IMGcrop.py
+++ IMGcrop.py
@@ -1,30 +1,68 @@
+
+"""                                                                                   
+                                                            
+                             . ......                       
+                           ..........:.                     
+                 ...... ...............:........            
+                :.............................:::.          
+           .:===.....::::.....:::::.......::-::::-          
+     .::  :*+****+::------------------::::::---:-:          
+    +***#=%**#*#** .::-:::.::------------------:-:          
+    -#*******###%-          .:::::--:--:...:-:.             
+   .:+*++********#+        : +#--=#* .                      
+   +******++:+**##*+        -::.:*#=#.                      
+     +***+=#@+*%:%**=      =:--::+#+=+                      
+     :++***#**#:=+**#- ::-*-:==--*#=--=-:.                  
+      :*****#+=*=:-**%=+.-..-+::--#-:+++++                  
+        =*+--:  +:::***..:-:+=-==+-:::-:-#                  
+                =:::=***--..:----==+=+====*:                
+                +::::***+.--:-::::===*+====+.               
+                +::::+***%**--=-::-==++=+=++#               
+               @::::::****####+-=+*+*+*####*+*              
+               %:...::=***##**##+=-=*#*##**##%=-            
+               #.......-**********##************#==-        
+          :-=+**-.......-**************####+***+++**-       
+       .:++++****.........-+*******+===+*****===:-.         
+       ++++++=+-=-=-:::::::::-=+-===++*******#=:.           
+         ----.      -:=-----=:-   --==++=++++##==           
+                                        ----:               
+
+Script by Expedition
+
+This script returns quadrants of images, useful as close-ups.
+    
+"""
 from PIL import Image
  
 # Opens a image in RGB mode
-number = 2256
-im = Image.open(r"F:\Pokemon\Processing\IMG_"+str(number)+".jpg")
- 
-# Size of the image in pixels (size of original image)
-# (This is not mandatory)
-width, height = im.size
- 
-# Setting the points for cropped image
+number      = 2256 # Directory where input image(s) is located
+number2     = 1903 # If only 1 image, same as number. If attempting multiple images of ascending image number, final image number
+directory   = "F:\\Pokemon\\Processing\\" # Directory where input image(s) is located
+out_dir     = "F:\\Pokemon\\Processing\\" # Directory where the result is saved to
 
-left = 0
-top = 0
-right = width / 2
-bottom = height / 2
+
+for picture in range(number,number2+1):
+    try:
+        im = Image.open(directory+"IMG_"+str(number)+".jpg")
  
-# Crop immage into 4 quadrants
-im1 = im.crop((left, top, right, bottom))
-im2 = im.crop((left, bottom, right, height))
-im3 = im.crop((right, top, width, bottom))
-im4 = im.crop((right, bottom, width, height))
-
-im1 = im1.save(r"F:\Pokemon\Processing\IMG_"+str(number)+"-1.jpg")
-im2 = im2.save(r"F:\Pokemon\Processing\IMG_"+str(number)+"-2.jpg")
-im3 = im3.save(r"F:\Pokemon\Processing\IMG_"+str(number)+"-3.jpg")
-im4 = im4.save(r"F:\Pokemon\Processing\IMG_"+str(number)+"-4.jpg")
-# Shows the image in image viewer
-#im1.show()
-
+        # Size of the image in pixels (size of original image)
+        width, height = im.size
+         
+        # Setting the points for cropped image
+        left = 0
+        top = 0
+        right = width / 2
+        bottom = height / 2
+         
+        # Crop immage into 4 quadrants
+        im1 = im.crop((left, top, right, bottom))
+        im2 = im.crop((left, bottom, right, height))
+        im3 = im.crop((right, top, width, bottom))
+        im4 = im.crop((right, bottom, width, height))
+        
+        im1 = im1.save(out_dir+"IMG_"+str(number)+"-1.jpg")
+        im2 = im2.save(out_dir+"IMG_"+str(number)+"-2.jpg")
+        im3 = im3.save(out_dir+"IMG_"+str(number)+"-3.jpg")
+        im4 = im4.save(out_dir+"IMG_"+str(number)+"-4.jpg")
+    except:
+        print("Unsuccesful with IMG_"+str(picture)+" , better luck next time!")
